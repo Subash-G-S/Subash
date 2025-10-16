e70413aed00f73c637359e1b48cc71b703c6249d
Subash <subashgopi2006@gmail.com>
2025-10-16 21:48:06 +0530

    added function for offline page

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,3 +10,17 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   // You can later add caching logic here
 });
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('ahaar-cache').then((cache) => {
+      return cache.addAll(['/', '/offline.html']);
+    })
+  );
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match('/offline.html'))
+  );
+});
